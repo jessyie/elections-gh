e2486d681ad1cd1f9c7856dbb5b2ceb58800f4d5
Jesse Kwame Ahado <jessedabankah@gmail.com>
2024-11-13 22:57:18 +0000

    mime4

--- geospatialproject/settings.py
+++ geospatialproject/settings.py
@@ -172,7 +172,9 @@ STATICFILES_FINDERS = [
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR/"staticfiles"
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
