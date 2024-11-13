f8c42098eb5f71501b8220bdb1ff1285e819db03
Jesse Kwame Ahado <jessedabankah@gmail.com>
2024-11-13 01:30:25 +0000

    ymlTest

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+#!/bin/bash
 
-pip install -r requirements.txt
+# Initialize conda
+eval "$(conda shell.bash hook)"
 
-python manage.py collectstatic --no-input
+# Create or update the environment from environment.yml
+conda env update -f environment.yml
+
+# Activate the environment
+conda activate mydjangoP
+
+# Run Django migrations
 python manage.py migrate
+
+# Install any additional pip packages from requirements.txt if needed
+# pip install -r requirements.txt
+
+# Collect static files (if applicable)
+python manage.py collectstatic --noinput
